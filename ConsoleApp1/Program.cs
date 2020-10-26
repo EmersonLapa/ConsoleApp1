3f7a684030a80420875b0fdedaab9f24953f9235
Emerson <emersonlapa@outlook.com>
2020-10-25 22:31:18 -0300

    Alterado uma condição

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            // Condicional Ternária
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("1 - Corsa || 2 - Gol");
+            int id = int.Parse(Console.ReadLine());
+
+            string carro = (id == 1) ? carro = "Corsa" : carro = "GOL";
+
+            Console.WriteLine(carro);
+          
+        }
+    }
+}
